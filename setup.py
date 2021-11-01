2dff746851580a576d126858d9c6e29f0e1e8ec2
Andy McKay <andymckay@github.com>
2021-11-01 11:14:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
 
 setup(
     name="actions-ips",
-    version="0.3.5",
+    version="0.3.6",
     description="A list of the GitHub Actions IP Addresses",
     author="Andy McKay",
     author_email="andymckay@github.com",
